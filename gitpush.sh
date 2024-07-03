befee6b5f58be2cda75eb3d9754aa819cdeb5e7a
zaramson <sonsj98@naver.com>
2024-07-03 10:14:01 +0900

    comment

--- gitpush.sh
+++ gitpush.sh
@@ -10,7 +10,7 @@ expect eof
 
 spawn git push -u origin +master
 expect "Username for 'https://github.com':"
-send "zaram_training\r"
+#send "zaram_training\r"
 
 #expect "Password for 'https://zaram_training@github.com':"
 #send "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\r"
