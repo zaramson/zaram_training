968d3c4908f891713a8ebb9d1b51a5a780b162e4
zaramson <sonsj98@naver.com>
2024-07-03 10:12:52 +0900

    comment

---
+++ gitpush.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/expect
+
+set timeout -1
+
+spawn git add .
+expect eof
+
+spawn git commit -m "comment"
+expect eof
+
+spawn git push -u origin +master
+expect "Username for 'https://github.com':"
+send "zaram_training\r"
+
+expect "Password for 'https://zaram_training@github.com':"
+send "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\r"
+
+interact
