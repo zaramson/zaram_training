a71f1174a6f8151b16e432ff6284cda6d983df8c
zaramson <sonsj98@naver.com>
2024-07-03 10:39:55 +0900

    comment

--- gitpush.sh
+++ gitpush.sh
@@ -1,16 +1,4 @@
 #!/usr/bin/expect
 
-set timeout -1
 
-system git add .
-
-system git commit -m "comment"
-
-spawn git push -u origin +master
-expect "Username for 'https://github.com':"
-send "zaram_training\r"
-
-expect "Password for 'https://zaram_training@github.com':"
-send "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\r"
-
-interact
+source ~/gitpush_passwd.sh
